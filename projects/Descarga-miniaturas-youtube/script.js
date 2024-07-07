b9947c5b384abc83b999e7315586d24449ed215c
Franco Moreira <hidoggy.music@gmail.com>
2024-07-07 02:39:53 -0300

    Documentando un poco el codigo

--- projects/Descarga-miniaturas-youtube/script.js
+++ projects/Descarga-miniaturas-youtube/script.js
@@ -1,29 +1,33 @@
-// definiendo las constantes
-const urlUser = document.querySelector("#primary-input"); // texto que mete el usuario
-const botonOk = document.getElementById("primary-button"); // boton principal, el de OK
-const botonCopiar = document.getElementById("copiar-button");
+// Definiendo constantes y/o variables
+const urlUser = document.querySelector("#primary-input"); // texto que ingresa el usuario
+const botonOk = document.getElementById("primary-button"); // boton 'OK' principal
+const botonCopiar = document.getElementById("copiar-button"); // boton 'Copiar'
 const checkbox720p = document.getElementById('check1'); // checkbox de 720p
-const thumbPreview = document.getElementById("thumbnail-preview");
-const resultado = document.querySelector('#resultado');
 
-// eventos
-checkbox720p.addEventListener('click', checkOpcionMarkdown)
-botonOk.addEventListener('click', concatenarURL);
-botonCopiar.addEventListener('click', copiandoTexto);
+const thumbPreview = document.getElementById("thumbnail-preview"); // Espacio donde se imprimirá la img conseguida
+const resultado = document.querySelector('#resultado'); // Texto <p> que se imprime con el link de descarga
 
 let checkBoxOnOff = true;
 let thumbImg;
 
+// Eventos
+botonOk.addEventListener('click', concatenarURL);
+checkbox720p.addEventListener('click', checkOpcionMarkdown);
+botonCopiar.addEventListener('click', copiandoTexto);
+
 
 function getYTvideoId(url) {
-    const urlObj = new URL(url);
-    if (urlObj.hostname === 'youtu.be') {
+    const urlObj = new URL(url); // Creamos un objeto URL con la URL proporcionada
+    if (urlObj.hostname === 'youtu.be') { // Verificamos si la URL apunta a YouTube (youtu.be)
+        // El identificador del video se encuentra en la parte del pathname después de la ruta base 'youtu.be'
         return urlObj.pathname.slice(1);
-    } else if (urlObj.hostname.includes('youtube.com')) {
+    } else if (urlObj.hostname.includes('youtube.com')) { // Verificamos si la URL apunta a YouTube.com
+        // El identificador del video se encuentra en los parámetros de búsqueda con la clave 'v'
         return urlObj.searchParams.get('v');
     }
-    return null;
+    return null; // Si no se cumple ninguna de las condiciones anteriores, devolvemos null
   }
+
   
 function concatenarURL() {
     const url = urlUser.value;
@@ -43,7 +47,7 @@ function concatenarURL() {
     botonCopiar.style.display = "flex";
     urlUser.style.display = "none";
     botonOk.style.display = "none";
-    document.getElementById('checkbox-and-p').style.display = "none";
+    document.getElementById('checkbox-and-p').style.display = "none"; // oculta checkbox720p y el texto p
     thumbPreview.setAttribute("src", result);
     thumbPreview.style.display = "flex";
 
